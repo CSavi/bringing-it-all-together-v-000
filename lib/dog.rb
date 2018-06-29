5743e4bd24da01a17720fd844315051cc78931ce
Carrie Savinell <carrielane89@gmail.com>
2018-06-29 19:03:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,10 +68,11 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0]
-      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
       dog_data = Dog.create(name: name, breed: breed)
     end
+    dog
   end
 
   def self.find_by_name(name)                          #ask tech coach about this method; explain result variable
